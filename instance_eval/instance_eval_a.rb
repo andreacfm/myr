81ee1df5bd56da24c74b9f025ea990a09df87e08
Andrea Campolonghi <acampolonghi@gmail.com>
2011-07-06 15:54:55 +0200

    comparable enumerable

--- instance_eval/instance_eval_a.rb
+++ instance_eval/instance_eval_a.rb
@@ -1,23 +1,23 @@
-module Andrea  
-  
-  module My_module
-    
+    module Andrea  
+
+    module My_module
+
     def self.configure
       yield
     end
-    
+
     def self.set(arg)
       puts arg
     end
-    
-  end  
-  
-end
 
-#anywhere in my program initilizers
-include Andrea
+    end  
 
-My_module.configure do
-  set "my setting"
-end  
+    end
+
+    #anywhere in my program initilizers
+    include Andrea
+
+    My_module.configure do
+    set "my setting"
+    end  
 
